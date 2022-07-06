d8e8a5a0e02f22f7f664aa93ad52ddbe79b6c042
julianstreibel <julian.streibel@freenet.de>
2022-07-06 18:54:08 +0200

    refactor masking app

--- masking.py
+++ masking.py
@@ -1,65 +1,61 @@
 import cv2
 import numpy as np
 
-# variables
-ix = -1
-iy = -1
-drawing = False
-img = False
-img_mask = False
 
+class Mask_Operator:
+    def __init__(self, input_path, output_path, file_name, local_avg_radius=5):
+        self.img = cv2.imread(input_path + "/" + file_name)
+        self.img_mask = np.zeros_like(self.img)
+        self.ix = -1
+        self.iy = -1
+        self.drawing = False
+        self.local_avg_radius = local_avg_radius
+        self.local_avg_color = self.img.mean(axis=(0, 1))
+        cv2.namedWindow("Ceate_Mask", 0)
+        cv2.resizeWindow("Ceate_Mask", 1500, 1500)
+        cv2.setMouseCallback("Ceate_Mask",
+                             self.draw_rectangle_with_drag)
 
-def draw_rectangle_with_drag(event, x, y, flags, param):
+        while True:
+            cv2.imshow("Ceate_Mask", self.img)
 
-    global ix, iy, drawing, img
+            if cv2.waitKey(10) == 27:
+                cv2.imwrite(input_path + "/" +
+                            file_name[:-4] + "_edit" + file_name[-4:], self.img)
+                cv2.imwrite(output_path + "/" + file_name[:-4] + "_edit_mask" +
+                            file_name[-4:], self.img_mask)
+                break
+        cv2.destroyAllWindows()
 
-    if event == cv2.EVENT_LBUTTONDOWN:
-        drawing = True
-        ix = x
-        iy = y
+    def draw_rectangle_with_drag(self, event, x, y, flags, param):
 
-    elif event == cv2.EVENT_MOUSEMOVE:
-        if drawing == True:
-            cv2.rectangle(img, pt1=(ix, iy),
+        if event == cv2.EVENT_LBUTTONDOWN:
+            self.drawing = True
+            self.ix = x
+            self.iy = y
+            self.local_avg_color = self.img[
+                self.iy - self.local_avg_radius: self.iy + self.local_avg_radius,
+                self.ix - self.local_avg_radius: self.ix + self.local_avg_radius
+            ].mean(axis=(0, 1))
+
+        elif event == cv2.EVENT_MOUSEMOVE:
+            if self.drawing == True:
+                cv2.rectangle(self.img, pt1=(self.ix, self.iy),
+                              pt2=(x, y),
+                              color=self.local_avg_color,
+                              thickness=-1)
+                cv2.rectangle(self.img_mask, pt1=(self.ix, self.iy),
+                              pt2=(x, y),
+                              color=(255, 255, 255),
+                              thickness=-1)
+
+        elif event == cv2.EVENT_LBUTTONUP:
+            self.drawing = False
+            cv2.rectangle(self.img, pt1=(self.ix, self.iy),
                           pt2=(x, y),
-                          color=img.mean(axis=(0, 1)),
+                          color=self.local_avg_color,
                           thickness=-1)
-            cv2.rectangle(img_mask, pt1=(ix, iy),
+            cv2.rectangle(self.img_mask, pt1=(self.ix, self.iy),
                           pt2=(x, y),
                           color=(255, 255, 255),
                           thickness=-1)
-
-    elif event == cv2.EVENT_LBUTTONUP:
-        drawing = False
-        cv2.rectangle(img, pt1=(ix, iy),
-                      pt2=(x, y),
-                      color=img.mean(axis=(0, 1)),
-                      thickness=-1)
-        cv2.rectangle(img_mask, pt1=(ix, iy),
-                      pt2=(x, y),
-                      color=(255, 255, 255),
-                      thickness=-1)
-
-
-def mask_image(input_path, output_path, file_name):
-
-    global ix, iy, drawing, img, img_mask
-
-    img = cv2.imread(input_path + "/" + file_name)
-    img_mask = np.zeros_like(img)
-
-    cv2.namedWindow("Ceate_Mask", 0)
-    cv2.resizeWindow("Ceate_Mask", 1500, 1500)
-    cv2.setMouseCallback("Ceate_Mask",
-                         draw_rectangle_with_drag)
-
-    while True:
-        cv2.imshow("Ceate_Mask", img)
-
-        if cv2.waitKey(10) == 27:
-            cv2.imwrite(input_path + "/" +
-                        file_name[:-4] + "_edit" + file_name[-4:], img)
-            cv2.imwrite(output_path + "/" + file_name[:-4] + "_edit_mask" +
-                        file_name[-4:], img_mask)
-            break
-    cv2.destroyAllWindows()
--- masking_app.py
+++ masking_app.py
@@ -1,21 +1,20 @@
 import tkinter as tk
-from tkinter import ttk
 import os
-from masking import mask_image
-import time
+from masking import Mask_Operator
 import subprocess
 
 
 class Application(tk.Frame):
     def __init__(self,
                  master=None,
-                 input_masking_path="../SinGAN_antyang/Input/Images",
-                 input_training_path="../SinGAN_antyang/Input/Images",
-                 output_mask_path="../SinGAN_antyang/Input/Inpainting",
-                 input_model_path="../SinGAN_antyang/TrainedModels"
+                 input_masking_path="input/images",
+                 input_training_path="input/images",
+                 output_mask_path="input/editing",
+                 input_model_path="models"
                  ):
         super().__init__(master)
         self.master = master
+        self.master.title("Masking App")
         self.input_masking_path = input_masking_path
         self.output_mask_path = output_mask_path
         self.input_training_path = input_training_path
@@ -26,36 +25,11 @@ class Application(tk.Frame):
             self)
         self.picked_input_masking.set(self.input_masking_option_list[0])
 
-        # training
-        self.input_training_option_list = os.listdir(input_training_path)
-        self.picked_input_training = tk.StringVar(
-            self)
-        self.picked_input_training.set(self.input_training_option_list[0])
-
-        # inpainting
-        self.picked_input_inpainting = tk.StringVar(
-            self)
-        self.picked_input_inpainting.set(self.input_masking_option_list[0])
-
-        self.input_inpainting_models_option_list = os.listdir(input_model_path)
-        self.picked_input_inpainting_model = tk.StringVar(
-            self)
-        self.picked_input_inpainting_model.set(
-            self.input_inpainting_models_option_list[0])
-
         self.pack()
         self.create_widgets()
 
-    def run_train(self):
-        p = subprocess.Popen(
-            f"python3 main_train.py --inpainting --input_name {self.picked_input_training.get()}", shell=True)
-        self.pb.start()
-        while p.poll() is None:
-            self.pb.update()
-        self.pb.stop()
-
     def run_mask_image(self):
-        mask_image(
+        Mask_Operator(
             self.input_masking_path,
             self.output_mask_path,
             self.picked_input_masking.get())
@@ -82,52 +56,6 @@ class Application(tk.Frame):
         self.create_mask_button["text"] = "Create Mask"
         self.create_mask_button["command"] = self.run_mask_image
         self.create_mask_button.pack(side="top")
-
-        # training
-        self.input_training_label = tk.Label(
-            self, text="Training", font=('Helvetica', 20))
-        self.input_training_label.pack(side="top")
-
-        self.input_training_dropdown = tk.OptionMenu(
-            self,
-            self.picked_input_training,
-            *self.input_training_option_list)
-        self.input_training_dropdown.config(width=90, font=('Helvetica', 12))
-        self.input_training_dropdown.pack(side="top")
-
-        self.training_button = tk.Button(self)
-        self.training_button["text"] = "Train"
-        self.training_button["command"] = self.run_train
-        self.training_button.pack(side="top")
-
-        self.pb = ttk.Progressbar(self, mode="indeterminate")
-        self.pb.pack(side="top")
-
-        # inpainting
-        self.input_inpainting_label = tk.Label(
-            self, text="Inpainting", font=('Helvetica', 20))
-        self.input_inpainting_label.pack(side="top")
-
-        #   input
-        self.input_inpainting_dropdown = tk.OptionMenu(
-            self,
-            self.picked_input_inpainting,
-            *self.input_masking_option_list)
-        self.input_inpainting_dropdown.config(width=90, font=('Helvetica', 12))
-        self.input_inpainting_dropdown.pack(side="top")
-
-        #   model
-        self.input_inpainting_dropdown = tk.OptionMenu(
-            self,
-            self.picked_input_inpainting_model,
-            *self.input_inpainting_models_option_list)
-        self.input_inpainting_dropdown.config(width=90, font=('Helvetica', 12))
-        self.input_inpainting_dropdown.pack(side="top")
-
-        self.inpainting_button = tk.Button(self)
-        self.inpainting_button["text"] = "Inpaint"
-        self.inpainting_button["command"] = lambda: print("inpaint")
-        self.inpainting_button.pack(side="top")
 
 
 root = tk.Tk()

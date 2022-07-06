c6f6da7b37f743be53c08d704812d568f32ccbbf
julianstreibel <julian.streibel@freenet.de>
2022-07-06 18:15:58 +0200

    updated poster

---
+++ paint2image_script.py
@@ -0,0 +1,23 @@
+import subprocess
+
+
+input_names = [
+    "image_0007a_small_edit.png",
+    "image_0004_small_edit.png",
+    "image_0006_small_edit.png",
+    "image_0015_small_edit.png",
+    "image_0004_mid_edit.png",
+    "image_0015_mid_edit.png"
+]
+
+
+# predict on every scale
+for input_name in input_names:
+    for scale in range(1, 10):
+        p = subprocess.Popen(
+            f"python3 paint2image.py --input_name {input_name} --ref_dir Input/Images --ref_name {input_name} --paint_start_scale {scale}", # --quantization_flag
+            shell=True
+        )
+        p.wait()
+
+print("done")
--- train_script.py
+++ train_script.py
@@ -2,11 +2,11 @@ import subprocess
 
 
 input_names = [
-    # "image_0007a_small_edit.png",
-    # "image_0004_small_edit.png",
-    # "image_0006_small_edit.png",
-    # "image_0015_small_edit.png",
-    # "image_0004_mid_edit.png",
+    "image_0007a_small_edit.png",
+    "image_0004_small_edit.png",
+    "image_0006_small_edit.png",
+    "image_0015_small_edit.png",
+    "image_0004_mid_edit.png",
     "image_0015_mid_edit.png"
 ]
 
@@ -17,14 +17,5 @@ for input_name in input_names:
         shell=True
     )
     p.wait()
-
-# predict on every scale
-for input_name in input_names:
-    for scale in range(1, 10):
-        p = subprocess.Popen(
-            f"python3 inpainting.py --input_name {input_name} --inpainting_start_scale {scale}",
-            shell=True
-        )
-        p.wait()
 
 print("done")
